e7f8f3adf4e35e90471e51ae494d3d68c9e4ce3f
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-25 17:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function delayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById("output");
+
+delayHello().then((message) => {
+  outputElement.textContent = message;
+});
